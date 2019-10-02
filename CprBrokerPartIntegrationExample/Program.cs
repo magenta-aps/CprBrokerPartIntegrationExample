338d606d07ef3ebcfaa25310dd004f2ce068cac9
heiniovason <heini.ovason@gmail.com>
2019-10-02 14:24:10 +0200

    initial commit

---
+++ CprBrokerPartIntegrationExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CprBrokerPartIntegrationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
